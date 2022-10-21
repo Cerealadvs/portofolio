99903ef46cbc25baf4da1b3855a74ede29c4b0fa
cerealadvs <cyrildelteil@gmail.com>
2022-10-21 07:59:45 +0200

    correction alt aboutme + typescript

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -16,6 +16,7 @@ import seo from '../images/seo.svg'
 import vite from '../images/vite.svg'
 import next from '../images/next.svg'
 import svelte from '../images/svelte.svg'
+import typescript from '../images/typescript.svg'
 
 const AboutMe = () => {
   return (
@@ -41,68 +42,68 @@ const AboutMe = () => {
       <div className='lg:flex gap-10 shadow-lg p-10 rounded-xl my-2 dark: shadow-slate-600'>
         <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-2 place-items-center'>
           <div>
-            <img src={mongo} width={100} height={100} alt='schema' />
+            <img src={mongo} width={100} height={100} alt='mongo' />
             <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
               mongo.db
             </p>
           </div>
 
           <div>
-            <img src={express} width={100} height={100} alt='schema' />
+            <img src={express} width={100} height={100} alt='express' />
             <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
               express
             </p>
           </div>
           <div>
-            <img src={react} width={100} height={100} alt='schema' />
+            <img src={react} width={100} height={100} alt='react' />
             <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
               react
             </p>
           </div>
           <div>
-            <img src={node} width={100} height={100} alt='schema' />
+            <img src={node} width={100} height={100} alt='node' />
             <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
               node.js
             </p>
           </div>
           <div>
-            <img src={tailwind} width={100} height={100} alt='schema' />
+            <img src={tailwind} width={100} height={100} alt='tailwind' />
             <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
               tailwind
             </p>
           </div>
           <div>
-            <img src={html} width={100} height={100} alt='schema' />
+            <img src={html} width={100} height={100} alt='html' />
             <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
               html
             </p>
           </div>
           <div>
-            <img src={css} width={100} height={100} alt='schema' />
+            <img src={css} width={100} height={100} alt='css' />
             <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
               css
             </p>
           </div>
           <div>
-            <img src={sass} width={100} height={100} alt='schema' />
+            <img src={sass} width={100} height={100} alt='sass' />
             <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
               sass
             </p>
           </div>
           <div>
-            <img src={js} width={100} height={100} alt='schema' />
+            <img src={js} width={100} height={100} alt='js' />
             <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
               javascript
             </p>
           </div>
           <div>
-            <img src={api} width={100} height={100} alt='schema' />
+            <img src={api} width={100} height={100} alt='api' />
             <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
               api rest
             </p>
           </div>
           <div>
-            <img src={seo} width={100} height={100} alt='schema' />
+            <img src={seo} width={100} height={100} alt='seo' />
             <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
               référencement
             </p>
@@ -117,22 +118,28 @@ const AboutMe = () => {
       <div className='lg:flex gap-10 shadow-lg p-10 rounded-xl my-2 dark: shadow-slate-600'>
         <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-2 place-items-center'>
           <div>
-            <img src={vite} width={100} height={100} alt='schema' />
+            <img src={vite} width={100} height={100} alt='vite' />
             <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
               vite
             </p>
           </div>
 
           <div>
-            <img src={next} width={100} height={100} alt='schema' />
+            <img src={next} width={100} height={100} alt='next' />
             <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
               next
             </p>
           </div>
           <div>
-            <img src={svelte} width={100} height={100} alt='schema' />
+            <img src={svelte} width={100} height={100} alt='svelte' />
             <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
               svelte
+            </p>
+          </div>
+          <div>
+            <img src={typescript} width={100} height={100} alt='typescript' />
+            <p className=' text-center text-md py-2 leading-8 text-gray-800 font-burtons dark:text-slate-400'>
+              typescript
             </p>
           </div>
         </div>
